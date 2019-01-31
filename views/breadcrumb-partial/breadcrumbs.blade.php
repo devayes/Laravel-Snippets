864c6998e5467fb85d42d2e6b48089e449f4c920
Devin Hayes <devayes@gmail.com>
2019-01-31 12:50:23 -0500

    formatting

--- views/breadcrumb-partial/breadcrumbs.blade.php
+++ views/breadcrumb-partial/breadcrumbs.blade.php
@@ -1,15 +1,15 @@
 @if($breadcrumbs)
-@section('breadcrumbs')
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        @foreach($breadcrumbs as $bc)
-            @if( ! empty($bc['href']))
-                <li class="breadcrumb-item"><a href="{{ $bc['href'] }}">{{ $bc['text'] }}</a></li>
-            @else
-                <li class="breadcrumb-item active" aria-current="page">{{ $bc['text'] }}</li>
-            @endif
-        @endforeach
-    </ol>
-</nav>
-@endsection
+    @section('breadcrumbs')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            @foreach($breadcrumbs as $bc)
+                @if( ! empty($bc['href']))
+                    <li class="breadcrumb-item"><a href="{{ $bc['href'] }}">{{ $bc['text'] }}</a></li>
+                @else
+                    <li class="breadcrumb-item active" aria-current="page">{{ $bc['text'] }}</li>
+                @endif
+            @endforeach
+        </ol>
+    </nav>
+    @endsection
 @endif
